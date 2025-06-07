d96c0bdafb8617f3fd8a20813967ba48582c146d
lssb2003 <lssb2003@gmail.com>
2025-06-07 20:02:01 +0700

    Initial commit

---
+++ next.config.ts
@@ -0,0 +1,8 @@
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+    reactStrictMode: true,
+    // Add other config options here as needed
+}
+
+export default nextConfig
