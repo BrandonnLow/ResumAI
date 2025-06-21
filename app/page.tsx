1164e40cfc6d459efc0912ce3c512753498fa861
lssb2003 <lssb2003@gmail.com>
2025-06-21 20:30:54 +0700

    Added hero to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
 'use client'
 
+import Hero from "./ui/Hero/Hero";
+
 export default function Home() {
     return (
-        <div className="min-h-screen bg-gray-700 flex items-center justify-center">
-            <div className="text-center">
-                <h1 className="text-4xl font-bold text-white mb-4">
-                    PersonaPrep
-                </h1>
-                <p className="text-gray-300 text-lg">
-                    AI-powered interview preparation platform
-                </p>
-            </div>
+        <div className="bg-gray-700 min-h-screen">
+            <main className="flex flex-col w-full gap-[32px] row-start-2 items-center sm:items-start">
+                <Hero />
+            </main>
+            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+
+            </footer>
         </div>
     );
 }
